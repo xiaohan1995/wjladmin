e8b6417c18ebdef4a0027e0084d55859cef50c8b
xiaohan1995 <1192539415@qq.com>
2020-06-20 18:27:15 +0800

    look

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost', // 127.0.0.1
+    'hostname'        => '49.232.24.217', // 127.0.0.1
     // 数据库名
-    'database'        => 'yxadmin',
+    'database'        => 'wanjiale',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root', 
+    'password'        => 'parents123', 
     // 端口
     'hostport'        => '3306',
     // 连接dsn
